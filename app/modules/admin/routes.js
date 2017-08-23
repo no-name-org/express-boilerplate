41d75101132adb21ca0a03cc67b46cb341697eaa
Froy Terell C. De Leon <froy.deleon22@gmail.com>
2017-08-23 20:05:52 +0800

    Removed security and views from admin users module.

--- app/modules/admin/routes.js
+++ app/modules/admin/routes.js
@@ -1,8 +1,8 @@
 var router = require('express').Router();
 
-var authMiddleware = require('../auth/middlewares/auth');
+//Security and session
+//var authMiddleware = require('../auth/middlewares/auth');
 
-router.use(authMiddleware.hasAuth);
-router.use('/users', require('./users/routes'));
+router.use('/users', require('./users/api'));
 
 exports.admin = router;
